062e287d14146405ad16c3b5ee02c963f5a14a26
Pete <opletaevpetr@mail.ru>
2024-03-03 13:50:42 +0300

    Lanuage, genre, author, pages filters implemented

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,3 +1,5 @@
+// book-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { forkJoin } from 'rxjs';
 import { BookService } from '../../services/book.service';
@@ -12,6 +14,11 @@ export class BookListComponent implements OnInit {
   books: Book[] = [];
   filteredBooks: Book[] = [];
   selectedBook: Book | null = null;
+  languages: string[] = [];
+  genres: string[] = [];
+  authors: string[] = [];
+  minPages: number | null = null;
+  maxPages: number | null = null;
 
   constructor(private bookService: BookService) {}
 
@@ -26,6 +33,19 @@ export class BookListComponent implements OnInit {
     ]).subscribe(([booksResponse, authorsResponse]) => {
       this.books = booksResponse.map(book => {
         const author = authorsResponse.find(author => author.uuid === book.author_uuid);
+
+        if (author && !this.authors.includes(author.name)) {
+          this.authors.push(author.name);
+        }
+        
+        if (!this.languages.includes(book.language)) {
+          this.languages.push(book.language);
+        }
+
+        if (!this.genres.includes(book.genre)) {
+          this.genres.push(book.genre);
+        }
+
         return { ...book, author: author ? author.name : 'Unknown Author' };
       });
 
@@ -34,9 +54,10 @@ export class BookListComponent implements OnInit {
   }
 
   searchBooks(searchTerm: string): void {
+    console.log("SEACH EVENT")
     this.filteredBooks = this.books.filter(book =>
       book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      book.description.toLowerCase().includes(searchTerm.toLowerCase())  || 
+      book.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
       book.author.toLowerCase().includes(searchTerm.toLowerCase())
     );
   }
@@ -47,5 +68,28 @@ export class BookListComponent implements OnInit {
 
   closeBookPreview(): void {
     this.selectedBook = null;
+  }
+
+  filterBooks(filters: {
+    selectedAuthor: string,
+    selectedGenre: string,
+    selectedLanguage: string,
+    searchTerm: string,
+    minPages: number | null,
+    maxPages: number | null
+  }): void {
+    this.filteredBooks = this.books.filter(book => {
+      const authorMatch = !filters.selectedAuthor || book.author.toLowerCase() === filters.selectedAuthor.toLowerCase();
+      const genreMatch = !filters.selectedGenre || book.genre.toLowerCase() === filters.selectedGenre.toLowerCase();
+      const languageMatch = !filters.selectedLanguage || book.language.toLowerCase() === filters.selectedLanguage.toLowerCase();
+      const searchTermMatch = !filters.searchTerm ||
+        book.title.toLowerCase().includes(filters.searchTerm.toLowerCase()) ||
+        book.description.toLowerCase().includes(filters.searchTerm.toLowerCase()) ||
+        book.author.toLowerCase().includes(filters.searchTerm.toLowerCase());
+      const minPagesMatch = filters.minPages ? book.pages >= filters.minPages : true;
+      const maxPagesMatch = filters.maxPages ? book.pages <= filters.maxPages : true;
+      
+      return authorMatch && genreMatch && languageMatch && searchTermMatch && minPagesMatch && maxPagesMatch;
+    });
   }
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,16 +6,53 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
-  searchTerm: string = '';
+  @Input() selectedAuthor: string = '';
+  @Input() selectedGenre: string = '';
+  @Input() selectedLanguage: string = '';
+  @Input() searchTerm: string = '';
+  @Input() authors: string[] = [];
+  @Input() genres: string[] = [];
+  @Input() languages: string[] = [];
 
+  minPages: number | null = null;
+  maxPages: number | null = null;
+
+  @Output() filterChangeEvent = new EventEmitter();
   @Output() searchEvent = new EventEmitter<string>();
 
   clearSearch() {
     this.searchTerm = '';
     this.searchEvent.emit('');
+    this.emitFilterChanges();
   }
 
   search() {
     this.searchEvent.emit(this.searchTerm);
+    this.emitFilterChanges();
+  }
+
+  filterChange() {
+    this.emitFilterChanges();
+  }
+
+  emitFilterChanges() {
+    this.filterChangeEvent.emit({
+      selectedAuthor: this.selectedAuthor,
+      selectedGenre: this.selectedGenre,
+      selectedLanguage: this.selectedLanguage,
+      searchTerm: this.searchTerm,
+      minPages: this.minPages,
+      maxPages: this.maxPages,
+    });
+  }
+
+  resetFilters() {
+    this.selectedAuthor = '';
+    this.selectedGenre = '';
+    this.selectedLanguage = '';
+    this.searchTerm = '';
+    this.minPages = null;
+    this.maxPages = null;
+    this.emitFilterChanges();
   }
 }
